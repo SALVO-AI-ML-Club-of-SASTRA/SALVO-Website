0fa4af0b20cf7f4175f71ce8d729133c91d308b1
SALVO - The Official AI/ML Club of SASTRA <salvo.aics@gmail.com>
2025-04-02 12:45:10 +0530

    Initial commit 2 - added app website.
    Next steps would be to start defining models and views and kickstart the project.
    @KKeshav1101 shall be doing the same

--- salvo_website/salvo_website/urls.py
+++ salvo_website/salvo_website/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import home,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
+    path('home/',home),
+    path('login/',login),
 ]
