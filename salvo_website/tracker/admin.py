cbb2f3044045dc158303fa248269c0b5e5339200
Dhakshin A V <avdhakshin1354@gmail.com>
2025-06-27 10:51:17 +0530

    Created a basic react app

--- salvo_website/tracker/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Attendance,Meeting,Member,Contribution
-# Register your models here.
-admin.site.register(Attendance)
-admin.site.register(Meeting)
-admin.site.register(Member)
-admin.site.register(Contribution)
